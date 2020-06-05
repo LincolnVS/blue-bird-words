8f2aa95142db5bf4147cf97a6d83d280c9540bcd
Lincoln V Schreiber <lincolnschreiber@gmail.com>
2020-06-05 00:56:13 -0300

    last update

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import datetime
 import numpy as np
 
-@st.cache(persist=True)
 def get_stopwords(linguagem = 'pt'):
   if linguagem == 'en':
     return ['on', "mightn't", 'yours', 't', 'myself', 'did', 'as', 'more', 'haven', 'very', 'are', 'again', 'both', 'against', "wasn't", 'some', 'does', "didn't", 'above', 'for', 'having', 'by', 'during', 'same', 'further', 'under', 'when', 'wasn', 'they', 'be', 'm', "doesn't", 'each', 'd', 'of', "that'll", 'you', 'herself', 'ours', 'doesn', "isn't", 'all', "won't", 'himself', "couldn't", 'which', 'have', 'that', 'before', 'into', 'so', 'your', 'who', 'these', 'then', 'she', 'didn', 'do', 'we', 'my', 'to', 'after', 'most', 'should', 'me', "haven't", 'them', 'once', 'with', 'has', "don't", 'other', "mustn't", 'were', 'from', "shouldn't", 'aren', 'this', "weren't", 'doing', 'mightn', "you'd", "you'll", 'he', 'or', 'between', "you've", 'own', 'weren', 'am', 'if', 'ourselves', 'about', 'no', 'ma', 'but', "she's", 'our', 'any', 'was', 'been', 'their', 'will', 'needn', 've', 'is', 'll', 'wouldn', 'through', 'what', 'shan', "shan't", 'hadn', "it's", 'why', 'won', 'itself', 'in', 'those', 'off', 'her', "should've", 'mustn', 'here', 'at', 'until', 'ain', 'nor', 'yourselves', 's', 'don', 'its', 'i', 'a', 'it', 'out', 'such', 'just', 're', "hadn't", 'themselves', 'had', 'an', 'shouldn', "needn't", 'y', 'yourself', 'than', 'not', 'the', "hasn't", 'where', 'up', 'hers', "you're", 'because', 'down', 'can', 'and', "aren't", 'his', 'now', 'theirs', 'while', 'being', 'only', 'below', 'hasn', 'o', 'whom', "wouldn't", 'isn', 'how', 'few', 'there', 'couldn', 'too', 'him', 'over']
@@ -57,6 +56,27 @@ def twitter_api(hashtag,num_tweets,linguagem):
     string_total_n_patterns =  string_total_n_patterns.replace("`","")
     return string_total_n_patterns
 
+def cria_nuvem_de_palavras(texto,word_cloud_space,collocations,background_color,custom_max_words,custom_stopwords):
+  import matplotlib.pyplot as plt
+  from wordcloud import WordCloud
+
+  stopwords = get_stopwords(linguagem)
+
+  print ("There are {} words in the combination of all review.".format(len(texto)))
+
+  # Create stopword list:
+  stopwords = set(stopwords)
+  stopwords.update(["tweet","twitter", "rt"])
+  stopwords.update(custom_stopwords)
+
+  # Generate a word cloud image
+  wordcloud = WordCloud(stopwords=stopwords, background_color=background_color,collocations = collocations,max_words=custom_max_words).generate(texto)
+
+  # Display the generated image:
+  # the matplotlib way:
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis("off")
+  word_cloud_space.pyplot()
 
 
 hashtag,num_tweets,linguagem = slidebar()
@@ -65,23 +85,23 @@ st.write("""
 # BLUE BIRD WORD 
 """)
 
-
-
 st.write('Criando nuvem de palavras com a hashtag: ', hashtag)
 st.write("Pegando ", num_tweets, ' tweets dessa hashtag na linguagem ', linguagem)
 texto = twitter_api(hashtag,num_tweets,linguagem)
-st.write('NUVEMM DE PALAVRAS VAI APARECER AQUIII')
 
+word_cloud_space = st.empty()
+#st.write('NUVEMM DE PALAVRAS VAI APARECER AQUIII')
 
 st.markdown('---')
 st.subheader('Parametros da nuvem de palavras:')
 
-custom_max_words = st.slider('Selecione o numero de palavras na nuvem de palavra', 20, 1000, 100)
+custom_max_words = st.slider('Selecione o numero de palavras na nuvem de palavra', 20, 300, 100)
 
 st.markdown(' ')
 ##Variaveis que o usuario vai poder editar wordcloud
 custom_stopwords = st.text_input('Stopwords customizadas separadas por virgula', str(hashtag[1:])+',') #Palavras que quer tirar da nuvem de palavras
-custom_stopwords = custom_stopwords.split(',')
+custom_stopwords = custom_stopwords.replace(' ','').split(',')
+
 st.write(custom_stopwords)
 st.markdown(' ')
 st.markdown(' ')
@@ -94,3 +114,6 @@ st.markdown(' ')
 background_color =  st.selectbox(
     'Qual cor de fundo',
      ['white','black','red','blue']) #cor de fundo so pode por enquanto 'black', 'white', 'red', 'blue'
+
+
+cria_nuvem_de_palavras(texto,word_cloud_space,collocations,background_color,custom_max_words,custom_stopwords)
